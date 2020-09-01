4966c792ff4d422a4f50868bfeba01ec477e4d85
Arthur <arthurarp@hotmail.com>
2020-09-01 02:50:05 -0300

    adicionando estrutura inicial do grafo com os metodos addNode e addEdge

---
+++ src/graph.js
@@ -0,0 +1,38 @@
+class Graph {
+
+    constructor() {
+        this.nodes = {};
+    }
+    
+    // em 'data' deve vir o JSON retornado pelo resultado do GET na api do GitHub.
+    addNode(data) {
+        let node = {
+            'image': data['avatar_url'], 
+            'neighbors': [], 
+        }
+
+        this.nodes[data['login']] = node;
+
+    }
+
+    addEdge(origin, destiny) {
+        this.nodes[origin]['neighbors'].push(destiny);
+    }
+
+    print() {
+    	console.log(this.nodes);
+        
+    }
+}
+
+
+/*
+Exemplo de JSON que deve vir em data:
+
+{
+  "login": "arthurarp",
+  "avatar_url": "https://avatars0.githubusercontent.com/u/31700115?v=4",
+}
+
+
+*/
