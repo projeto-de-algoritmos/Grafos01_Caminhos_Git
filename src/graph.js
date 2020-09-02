324fa7f8987ae2d3e4fa612da7c4be13c94621ca
Arthur <arthurarp@hotmail.com>
2020-09-02 03:21:53 -0300

    corrigindo bugs da BFS. (falta implementar a árvore de resposta da busca)

--- src/graph.js
+++ src/graph.js
@@ -20,32 +20,29 @@ class Graph {
   }
 
   bfs(origin, destiny) {
-    // let resulting_tree = {}
+    let resulting_tree = {};
     let queue = [];
+
     this.nodes[origin]['is_visited'] = true; // marcando o nó inicial da busca como visitado.
+
     for (var node of this.nodes[origin]['neighbors']) { // para todo nó de G
       if (this.nodes[node]['is_visited']) // ainda não explorado.
         continue; // se ja tiver explorado passa pro próximo.
       
       queue.push(node); // adicionando o nó ao final da fila.
       this.nodes[node]['is_visited'] = true; // marcando o nó como visitado.
-      console.log('>>>>>>>>>> fila', queue)
 
-      while (queue !== []) { // enquanto a fila nao estiver vazia.
+      while (queue.length != 0) { // enquanto a fila nao estiver vazia.
         let u = queue.shift(); // retira o primeiro da fila e salva em u.
-        console.log(u);
 
         for (let neighbor of this.nodes[u]['neighbors']) { // para cada vizinho de u.
           if (!this.nodes[neighbor]['is_visited']) { // se o vizinho de u, em questão, não foi visitado ainda então...
-            if (this.nodes[neighbor] == destiny)
-              return destiny + ' found!';
-              this.nodes[neighbor]['is_visited'] = true;
-              queue.push(neighbor);
-            /* var node_tree = {
-
-            }
-            resulting_tree[this.nodes[neighbor]['login']] =
-          */
+            
+            this.nodes[neighbor]['is_visited'] = true; // marcando nó atual como visitado.
+            queue.push(neighbor); // adicionando o nó atual à fila.
+            
+            if (this.nodes[neighbor]['login'] === destiny) // se o no atual for igual ao nó que estou procurando, retorna a árvore BFS.
+              return destiny + ' has found!';
             
           }
         }
