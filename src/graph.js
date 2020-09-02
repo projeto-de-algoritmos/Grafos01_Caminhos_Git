e2eadac35281225fb95cfe3ceaec3c8d146eed95
Arthur <arthurarp@hotmail.com>
2020-09-02 02:06:45 -0300

    adicionando funcao de busca em largura no grafo (ainda nao terminada a funcao)

--- src/graph.js
+++ src/graph.js
@@ -1,25 +1,68 @@
 class Graph {
-  constructor() {
+  constructor () {
     this.nodes = {};
   }
-
   // em 'data' deve vir o JSON retornado pelo resultado do GET na api do GitHub.
-  addNode(data) {
+  addNode (data) {
     let node = {
-      image: data['avatar_url'],
-      neighbors: [],
+      'login': data['login'],
+      'image': data['avatar_url'],
+      'neighbors': [],
+      'is_visited': false,
     };
 
     this.nodes[data['login']] = node;
+    console.log(this.nodes[data['login']])
   }
 
-  addEdge(origin, destiny) {
+  addEdge (origin, destiny) {
     this.nodes[origin]['neighbors'].push(destiny);
   }
 
-  print() {
-    console.log(this.nodes);
+  bfs(origin, destiny) {
+    // let resulting_tree = {}
+    let queue = [];
+    this.nodes[origin]['is_visited'] = true; // marcando o nó inicial da busca como visitado.
+    for (var node of this.nodes[origin]['neighbors']) { // para todo nó de G
+      if (this.nodes[node]['is_visited']) // ainda não explorado.
+        continue; // se ja tiver explorado passa pro próximo.
+      
+      queue.push(node); // adicionando o nó ao final da fila.
+      this.nodes[node]['is_visited'] = true; // marcando o nó como visitado.
+      console.log('>>>>>>>>>> fila', queue)
+
+      while (queue !== []) { // enquanto a fila nao estiver vazia.
+        let u = queue.shift(); // retira o primeiro da fila e salva em u.
+        console.log(u);
+
+        for (let neighbor of this.nodes[u]['neighbors']) { // para cada vizinho de u.
+          if (!this.nodes[neighbor]['is_visited']) { // se o vizinho de u, em questão, não foi visitado ainda então...
+            if (this.nodes[neighbor] == destiny)
+              return destiny + ' found!';
+              this.nodes[neighbor]['is_visited'] = true;
+              queue.push(neighbor);
+            /* var node_tree = {
+
+            }
+            resulting_tree[this.nodes[neighbor]['login']] =
+          */
+            
+          }
+        }
+
+      }
+    }
+
+    return 'not found!';
   }
+
+  print () {
+    for (var node in this.nodes) {
+      console.log(node);
+    }
+  }
+
+
 }
 
 /*
@@ -32,3 +75,4 @@ Exemplo de JSON que deve vir em data:
 
 
 */
+
