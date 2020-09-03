87d07874d42f598f6aeef49445b0f837fc0732d4
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-09-02 23:22:06 -0300

    populando grafo

--- src/BuscaGit.js
+++ src/BuscaGit.js
@@ -1,24 +1,40 @@
 import api from './api';
-
+import Graph from './graph';
 class BuscaGit {
   async index(req, res) {
-    const name = 'Andre-Eduardo';
+    const graph = new Graph();
+    let inicial = {
+      login: 'Andre-Eduardo',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/31700115?v=4',
+    }; // inicial
+    let name = 'Andre-Eduardo';
     const procurado = 'fepas';
+    let vizinhos = [];
     let achou = false;
-    let lista = [];
+    graph.addNode(inicial);
+    let cont = 0;
 
-    const response = await api.get(`/users/${name}/followers`);
+    while (cont <= 10) {
+      let response = await api.get(`/users/${name}/followers`);
 
-    response.data.map(user => {
-      lista.push(user.login);
-      if (user.login === procurado) {
-        achou = true;
-      }
-    });
-
-    console.log(lista);
-
+      response.data.map(user => {
+        let data = {
+          login: user.login,
+          avatar_url: user.avatar_url,
+        };
+        vizinhos.push(user.login);
+        graph.addNode(data);
+        graph.addEdge(inicial['login'], data['login']);
+      });
+      inicial = {
+        login: vizinhos[cont],
+      };
+      name = vizinhos[cont];
+      cont += 1;
+    }
+    console.log(vizinhos);
     return res.json('ola');
   }
 }
+
 export default new BuscaGit();
